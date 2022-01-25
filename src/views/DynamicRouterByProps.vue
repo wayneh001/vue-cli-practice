819f5ca0069cba131bdebebc0dd836eebdf56535
wayneh001 <wayneh001@yahoo.com.tw>
2022-01-25 14:23:01 +0800

    Update
    動態路由與 props

--- src/views/DynamicRouterByProps.vue
+++ src/views/DynamicRouterByProps.vue
@@ -4,9 +4,8 @@
 <script>
 import axios from 'axios'
 export default {
-  props: ['id'],
   created () {
-    console.log('props', this.id)
+    console.log(this.$route.params.id)
     const seed = this.$route.params.id
     axios.get(`https://randomuser.me/api/?seed=${seed}`).then(res => { console.log(res) })
   }
